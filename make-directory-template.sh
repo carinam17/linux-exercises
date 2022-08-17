01beefd87322280f5424778b46574fad345ccebc
carinam17 <45326544+carinam17@users.noreply.github.com>
2022-08-17 03:46:01 -0500

    Create make-directory-template.sh

---
+++ make-directory-template.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Description - A shell script that will:"
+    # - Accept a directory name
+    # - Create it as a subdirectory /home/vagrant/
+ 
+
+# A prompt for the file name
+# Asigned to the variable dirname
+echo "Enter the directory you would like to create: "
+
+read dirname
+
+
+# Complete the script below
+    # - Create directory /home/vagrant/{directory-name}
+    # - Use the appropriate bash command
+    # - Remember to include the appropriate directory path
+    # - The variable is referenced with $
+ 
+ 
+ 
