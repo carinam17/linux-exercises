64112323684b0b85946b19a90b51f0729080b1b4
carinam17 <45326544+carinam17@users.noreply.github.com>
2022-08-17 03:00:12 -0500

    Create copy-file-template.sh

---
+++ copy-file-template.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Description - A shell script that will:"
+    # - Accept a file name 
+    # - Accept the name of the copied file
+    # - Create a copy of the file in the directory /home/vagrant/my-scripts
+ 
+
+# A prompt for the file name
+# Asigned to the variable filename
+echo "Enter the file you would like to copy: "
+
+read filename
+
+# Complete the script below
+    # - Take the user input using read  
+    # - Assign it to the variable copyname
+
+echo "Enter the name of the copied file: "
+
+
+
+# Complete the script below
+    # - Copy the file to the directory /home/vagrant/my-scripts
+    # - Use the appropriate bash command
+    # - Remember to include the appropriate directory path
+    # - The variable is referenced with $
+    
+    
