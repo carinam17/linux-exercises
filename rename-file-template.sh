bd62a315bf7763e88bc2dbcbd08b0e206576b45b
carinam17 <45326544+carinam17@users.noreply.github.com>
2022-08-17 01:19:59 -0500

    Create rename-file-template.sh

---
+++ rename-file-template.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Description - A shell script that will:"
+    # - Accept a file name for a file that will be renamed
+    # - Accept a new name for the file
+    # - Rename the file
+    # - Prompt for confirmation before overwriting the file
+
+# A prompt for the file name
+echo "Enter the file you would like to rename: "
+
+# - Take the user input using read
+# -  Assign it to the variable oldname
+
+read oldname
+
+
+# Complete the script below #
+# - Prompt the user for the new file name
+# - Accept a new file name with the variable newname
+
+
+
+
+# Complete the script below #
+# - Rename the file to the new name
+# - Remember to specify a confirmation prompt
+# - The variable is referenced with a $
+
+
+
+
